2a9cc37f2ac7d38f73e9c6a7764729953e3619f0
nfwhite23 <137955750+nfwhite23@users.noreply.github.com>
2023-09-07 19:00:04 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,17 +10,17 @@ variable "key_name" {
 
 variable "instance_type" { 
     description = "instance type for ec2" 
-    default     =  "t2.small" 
+    default     =  "t2.micro" 
 }
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "launch-wizard-1" 
+    default     = "jenkins-sg-dec-2021" 
 }
 
 variable "tag_name" { 
     description = "Tag Name of for Ec2 instance" 
-    default     = "Jenkins-EC2" 
+    default     = "my-ec2-instance" 
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
